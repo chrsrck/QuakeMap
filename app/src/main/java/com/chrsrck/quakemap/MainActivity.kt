bf5ebc6aef0abe0967a9f10d978e88c69adadc87
chrsrck <chrsrck292@gmail.com>
2018-08-03 22:35:50 -0400

    Maps fragment appearing

--- app/src/main/java/com/chrsrck/quakemap/MainActivity.kt
+++ app/src/main/java/com/chrsrck/quakemap/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.chrsrck.quakemap
 
+import android.app.Fragment
+import android.app.ListFragment
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.maps.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+// google maps libraries
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    //private lateinit var mMap: GoogleMap
+
+    val SYDNEY = LatLng(-33.862, 151.21)
+    val ZOOM_LEVEL = 13f
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                //setUpListFragment()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +44,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setupMapFragment()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //mMap ?: googleMap
+        // Add a marker in Sydney and move the camera
+        googleMap ?: return
+        with(googleMap) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, ZOOM_LEVEL))
+            addMarker(MarkerOptions().position(SYDNEY))
+        }
+    }
+
+    private fun setupMapFragment() {
+        if (isOnline()) {
+            val mapFragment : SupportMapFragment? = SupportMapFragment.newInstance()
+            supportFragmentManager.beginTransaction().replace(R.id.container, mapFragment).commit()
+            mapFragment?.getMapAsync(this)
+        }
+        else {
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isOnline(): Boolean {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
     }
 }
