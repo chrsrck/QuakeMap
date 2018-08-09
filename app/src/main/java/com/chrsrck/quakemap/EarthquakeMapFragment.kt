031ec9962a9ef5e78626b21f777dbe1832d9deee
chrsrck <chrsrck292@gmail.com>
2018-08-09 16:54:36 -0400

    Binded quake fragment to viewmodel

--- app/src/main/java/com/chrsrck/quakemap/EarthquakeMapFragment.kt
+++ app/src/main/java/com/chrsrck/quakemap/EarthquakeMapFragment.kt
@@ -1,15 +1,22 @@
 package com.chrsrck.quakemap
 
+import android.app.Activity
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chrsrck.quakemap.databinding.EarthquakeMapFragmentBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import android.databinding.adapters.TextViewBindingAdapter.setText
+import com.google.android.gms.maps.model.MapStyleOptions
 
 
 class EarthquakeMapFragment : Fragment(), OnMapReadyCallback {
@@ -26,7 +33,17 @@ class EarthquakeMapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.earthquake_map_fragment, container, false)
+        //val view = inflater.inflate(R.layout.earthquake_map_fragment, container, false)
+        // Creating the binding and inflating the layout
+        // don't use DataBindingUtil since the layout binding is known in advance
+        viewModel = ViewModelProviders.of(this).get(EarthquakeViewModel::class.java)
+        val binding: EarthquakeMapFragmentBinding =
+                EarthquakeMapFragmentBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
+        binding.setLifecycleOwner(this)
+        val view = binding.root
+
+
         mapView = view.findViewById(R.id.mapView) as MapView
         mapView?.onCreate(savedInstanceState) // lifecycle method for memory leak prevention
         mapView?.getMapAsync(this)
@@ -35,15 +52,25 @@ class EarthquakeMapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(EarthquakeViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
+        val frag : EarthquakeMapFragment = this
         googleMap ?: return
         with(googleMap) {
             moveCamera(com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(SYDNEY, ZOOM_LEVEL))
             addMarker(com.google.android.gms.maps.model.MarkerOptions().position(SYDNEY))
+//            val mapObserver = Observer<Boolean> { darkMode ->
+//                // Update the UI, in this case, a TextView.
+//                if (darkMode!!) {
+//                    googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.dark_mode_style))
+//                }
+//                else {
+//                    googleMap.setMapStyle(MapStyleOptions("[]")) // "[]" is standard style
+//                }
+//            }
+//            viewModel.darkMode.observe(frag, mapObserver)
         }
     }
 
--- app/src/main/java/com/chrsrck/quakemap/EarthquakeViewModel.kt
+++ app/src/main/java/com/chrsrck/quakemap/EarthquakeViewModel.kt
@@ -1,7 +1,25 @@
 package com.chrsrck.quakemap
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel;
+import android.util.Log
 
 class EarthquakeViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    //var darkMode: LiveData<Boolean>
+    val darkMode: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    val modeText: MutableLiveData<String> = MutableLiveData<String>()
+
+    init {
+        darkMode.value = false
+        modeText.value = "Light mode"
+    }
+
+    fun onDarkModeClick() {
+        darkMode.value = (darkMode.value)?.not()
+        Log.d(this.javaClass.simpleName, "onDarkModeClicked")
+        modeText.value = if (darkMode.value!!) { "Dark mode" } else "Light mode"
+    }
+
 }
