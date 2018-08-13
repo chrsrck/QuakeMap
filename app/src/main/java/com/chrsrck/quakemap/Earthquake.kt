8a966cafbc594b77638b87e3e236f48fbf4effad
chrsrck <chrsrck292@gmail.com>
2018-08-13 16:46:45 -0400

    Refactored eq model object

--- app/src/main/java/com/chrsrck/quakemap/Earthquake.kt
+++ app/src/main/java/com/chrsrck/quakemap/Earthquake.kt
@@ -1,16 +1,20 @@
 package com.chrsrck.quakemap
 
-class Earthquake {
+class Earthquake(val magnitude: Double,
+                 val time: Double,
+                 val place: String,
+                 val longitude: Double,
+                 val latitude: Double) {
 
-    val magnitude: Double
-    val description: String
-    val latitude: Double
-    val longitude: Double
-
-    constructor(description: String, magnitude: Double, longitude: Double, latitude: Double) {
-        this.description = description
-        this.magnitude = magnitude
-        this.longitude = longitude
-        this.latitude = latitude
-    }
+//    val magnitude: Double
+//    val description: String
+//    val latitude: Double
+//    val longitude: Double
+//
+//    constructor(description: String, magnitude: Double, longitude: Double, latitude: Double) {
+//        this.description = description
+//        this.magnitude = magnitude
+//        this.longitude = longitude
+//        this.latitude = latitude
+//    }
 }
