c65fb2c74790c035b80e80c1e7fb3db8882e3135
chrsrck <chrsrck292@gmail.com>
2018-08-27 00:19:58 -0400

    Disabled selected bottom nav item

--- app/src/main/java/com/chrsrck/quakemap/MainActivity.kt
+++ app/src/main/java/com/chrsrck/quakemap/MainActivity.kt
@@ -4,10 +4,16 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.chrsrck.quakemap.databinding.ActivityMainBinding
 import com.chrsrck.quakemap.dummy.DummyContent
 import com.chrsrck.quakemap.ui.EarthquakeListFragment
@@ -26,18 +32,42 @@ class MainActivity : AppCompatActivity(), EarthquakeListFragment.OnListFragmentI
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 //                ActivityMainBinding.inflate(layoutInflater)
         binding.viewModel = viewModel
-        binding.bottomNavMenu.menu.getItem(0).isEnabled = false
+//        binding.bottomNavMenu.menu.getItem(0).isEnabled = false
+        val bottomNav = binding.bottomNavMenu as BottomNavigationView
 
 
         val navHostFragment =
                 supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
-        findViewById<BottomNavigationView>(R.id.bottom_nav_menu)?.let { bottomNavView ->
-            NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
-        }
+
+        NavigationUI.setupWithNavController(bottomNav, navHostFragment.navController)
+        bottomNav.setOnNavigationItemSelectedListener({ item ->
+                    item.onNavDestinationSelected(navHostFragment.navController)
+                })
+
+        navHostFragment.navController.addOnNavigatedListener(
+                NavController.OnNavigatedListener(
+                        fun (controller: NavController, dest : NavDestination) {
+                            val destId = dest.id
+                            val menu = bottomNav.menu
+                            for(i in 0.. menu.size() - 1) {
+                                val item : MenuItem = menu.getItem(i)
+                                if (item.itemId == destId) {
+                                    item.isEnabled = false
+                                }
+                                else {
+                                    item.isEnabled = true
+                                }
+                            }
+                        }
+                ))
     }
 
     override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
         Toast.makeText(this, "you clicked an item", Toast.LENGTH_SHORT).show()
+    }
+
+    fun setUpWithNavController() {
+
     }
 
 }
